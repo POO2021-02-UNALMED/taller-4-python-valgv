1d56362c48ad97b892644aadca69282f19110c9f
Valentina Gomez Valencia <begomezv@unal.edu.co>
2021-11-09 11:56:57 -0500

    Solucion taller

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,37 +1,41 @@
-
-
 from classroom.asignatura import Asignatura
 from classroom.asignatura import Asignatura
-
-
 class Grupo:
-
-    grado = None
-
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    grado = 12
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
+        list=[]
         for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+            list.append(x)
+        self._asignaturas = list
+            #self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    '''def agregarAlumno(self, alumno, lista=[]):
         if(lista is None):
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
-            self.listadoAlumnos = [alumno]
+            self.listadoAlumnos = [alumno]'''
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def agregarAlumno(self, alumno=None, lista=None):
+        if (lista is None):
+            lista=[]  
+            lista.append(alumno)   
+            self.listadoAlumnos = lista
+        elif(alumno!=None):
+            lista.append(alumno)
+            self.listadoAlumnos = lista  
+        else:
+            self.listadoAlumnos = lista   
+
+    def __str__(self) -> str:
+        grup = "Grupo de estudiantes: "+ self._grupo 
+        return grup          
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
